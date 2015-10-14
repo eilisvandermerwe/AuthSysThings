48681bb3ec8bf3193b959527a0a7a8ec03d0bb4c
eilisvandermerwe <eilis@vandermerwe.co.za>
2015-10-14 16:52:47 +0100

    can add user and register

--- src/app/AuthSysThings.java
+++ src/app/AuthSysThings.java
@@ -1,4 +1,6 @@
 package app;
+import java.util.Scanner;
+
 import app.UserAccess;
 
 public class AuthSysThings {
@@ -8,20 +10,51 @@ public class AuthSysThings {
 	}
 
 	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		UserAccess pleasework = new UserAccess();
+		
 		System.out.println("Hello, welcome to our app.");
 		System.out.println("If you want to login, press 1");
 		System.out.println("If you want to register, press 2");
 		
+		String result = scanner.nextLine();
 		
-		UserAccess pleasework = new UserAccess();
-		User bilbo = new User("Luke Skywalker", "LukeS", "force");
-		pleasework.saveUser(bilbo);
-		try {
-			User currentUser = pleasework.getUser ("LukeS" , "force");
-			System.out.println("Welcome " + currentUser.getName() + "!");
-		} catch (NoUserFoundEx e) {
-			System.out.println("User or password is WRONG.");
+		if(result.equals("1")){
+			// login our user
+
+			System.out.println("Enter your username:");
+			String username = scanner.nextLine();
+			
+			System.out.println("Enter your password:");
+			String password = scanner.nextLine();
+			
+			try {
+				User currentUser = pleasework.getUser (username, password);
+				System.out.println("Welcome " + currentUser.getName() + "!");
+			} catch (NoUserFoundEx e) {
+				System.out.println("User or password is WRONG.");
+			}
+			
+		} else if (result.equals("2")){
+			// register our user
+			System.out.println("Enter your name:");
+			String name = scanner.nextLine();
+			
+			System.out.println("Enter your username:");
+			String username = scanner.nextLine();
+			
+			System.out.println("Enter your password:");
+			String password = scanner.nextLine();
+
+			User bilbo = new User(name, username, password);
+			pleasework.saveUser(bilbo);
+			
+		} else {
+			// loop round again, idiot!
+			System.out.println("Ahhhh");
 		}
+
+		
 		
 	}
 
